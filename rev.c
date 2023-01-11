94100ce86d77cd7a1643561d96a51cf2050c5a7d
gopal awasthi <122447616+gopalawasthipandit@users.noreply.github.com>
2023-01-11 22:20:02 +0530

    Add files via upload

---
+++ rev.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+	int t,n;
+	printf("enter n=");
+	scanf("%d",&n);
+	int i,a[n];
+	for(i=0;i<n;i++){
+	printf("element");
+	scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    	printf("%d",a[i]);
+    	
+	}
+	printf("\n");
+	
+	
+	
+	
+
+}
